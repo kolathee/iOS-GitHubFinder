e33c59f5f1bd938388d42ca5a4daec1b54c40c56
kolathee <cupthee@gmail.com>
2017-04-06 12:56:06 +0700

    Initial Commit

---
+++ Mobile Challenge 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mobile Challenge 2
+//
+//  Created by Kolathee Payuhawatthana on 4/6/2560 BE.
+//  Copyright © 2560 Kolathee Payuhawattana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
